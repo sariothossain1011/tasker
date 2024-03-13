6d9deb7e8e32e10c05de8a7ce35e80f159767ac1
Sariot Hossain <sariothossain1011@gmail.com>
2024-03-13 22:24:18 +0600

    setup folder structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/header/Header'
+import HeroSection from './components/hero/HeroSection'
+import TaskSection from './components/task/TaskSection'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <HeroSection/>
+    <TaskSection/>
+    <Footer/>
+    </>
+
+  )
+}
+
+export default App
